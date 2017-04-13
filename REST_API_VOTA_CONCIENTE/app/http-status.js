095c588b2db70a926d6574b61e58de1c544bdbcd
yerfdude <yerflow@gmail.com>
2017-04-13 20:52:51 -0300

    Commit inicial VC

---
+++ REST_API_VOTA_CONCIENTE/app/http-status.js
@@ -0,0 +1,26 @@
+/*
+ * restApiVotaConciente - https://github.com/rest_api_vota_conciente/rest-api-vota-conciente.git
+ *
+ * Copyright (c) 2017 REST_API_VOTA_CONCIENTE
+ */
+
+/**
+ * Manages the sending of an promise callback.
+ *
+ * @module ravc/http-status
+ */
+
+'use strict';
+
+var httpStatus = {};
+
+Object.defineProperty(httpStatus, 'OKAY',           { value: 200, writable: false, enumerable: true});
+Object.defineProperty(httpStatus, 'BAD_REQUEST',    { value: 400, writable: false, enumerable: true});
+Object.defineProperty(httpStatus, 'UNAUTHORIZED',   { value: 401, writable: false, enumerable: true});
+Object.defineProperty(httpStatus, 'FORBIDDEN',      { value: 403, writable: false, enumerable: true});
+Object.defineProperty(httpStatus, 'NOT_FOUND',      { value: 404, writable: false, enumerable: true});
+Object.defineProperty(httpStatus, 'NOT_ALLOW',      { value: 405, writable: false, enumerable: true});
+Object.defineProperty(httpStatus, 'NOT_ACCEPTABLE', { value: 406, writable: false, enumerable: true});
+Object.defineProperty(httpStatus, 'SERVER_ERROR',   { value: 500, writable: false, enumerable: true});
+
+module.exports = httpStatus;
